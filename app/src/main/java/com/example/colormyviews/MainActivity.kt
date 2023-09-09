36c85c204799a29c6d3a0889e8f92e03ebe51704
Raefabdulnazir <raefnazir@gmail.com>
2023-09-09 23:50:59 +0400

    added setOnClickListener()

--- app/src/main/java/com/example/colormyviews/MainActivity.kt
+++ app/src/main/java/com/example/colormyviews/MainActivity.kt
@@ -18,9 +18,11 @@ class MainActivity : AppCompatActivity() {
             findViewById(R.id.box_three_text),
             findViewById(R.id.box_four_text),
             findViewById(R.id.box_five_text) ,
-            findViewById(R.id.constraint_layout)
-
-        )
+            findViewById(R.id.constraint_layout) ,
+            findViewById(R.id.green_btn) ,
+            findViewById(R.id.red_btn) ,
+            findViewById(R.id.yellow_btn)
+            )
         for (item in clickableViews) {
             item.setOnClickListener() {makeColoured(it)}
         }
@@ -30,9 +32,15 @@ class MainActivity : AppCompatActivity() {
         when(view.id){
             R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
             R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
             R.id.box_three_text -> view.setBackgroundColor(Color.BLUE)
             R.id.box_four_text -> view.setBackgroundColor(Color.RED)
             R.id.box_five_text -> view.setBackgroundColor(Color.GREEN)
+
+            R.id.green_btn -> box_three_text.setBackgroundResource(R.color.myGreen)
+            R.id.red_btn -> box_four_text.setBackgroundResource(R.color.myRed)
+            R.id.yellow_btn -> box_five_text.setBackgroundResource(R.color.myYellow)
+
             else -> view.setBackgroundColor(Color.LTGRAY)
 
         }
